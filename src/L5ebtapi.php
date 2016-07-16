639f37e964ac9280facef3016bfb02fd2f68dc34
Rod Shaffer <l5ebtapipkg@yahoo.com>
2016-07-15 21:16:22 -0400

    first commit

---
+++ src/L5ebtapi.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace l5ebtapi\l5ebtapi;
+
+class L5ebtapi
+{
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    /**
+     * Friendly welcome
+     *
+     * @param string $phrase Phrase to return
+     *
+     * @return string Returns the phrase passed in
+     */
+    public function echoPhrase($phrase)
+    {
+        return $phrase;
+    }
+}
